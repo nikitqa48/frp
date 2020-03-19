c551c382c2ac27ebdb1e18df054c9f31dc0d43ef
nikitqa48 <Princen1k@yandex.ru>
2020-03-19 17:15:56 +0300

    authoriza

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', ProfileView.as_view()),
+    path('aaa', hello)
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,24 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.decorators import api_view
 from .serializers import *
+from rest_framework.response import Response
+
+
 
 class ProfileView(CreateAPIView):
     serializer_class = ProfileSerializers
 
 
+@api_view(['GET', 'POST'])
+def hello(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+
+            profile = Profile.objects.get(user = request.user)
+            print(profile.phone)
+            context = {
+                profile: 'profile'
+            }
+            return Response (context)
+        else:
+            print('НЕ АВТОРИЗИРОВАН')
